d576c97dfa79fa392b81de30d2d4c8717b574c16
Diana Lazo <dlazo@ithaca.edu>
2025-01-26 19:37:55 -0500

    comments-Diana

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 
@@ -47,6 +48,43 @@ class BankAccountTest {
 
         
     }
+    @Test
+    void isEmailValidTestnotes(){
+        assertTrue(BankAccount.isEmailValid( "a@b.com"));
+        // Valid email address format
+        assertTrue(BankAccount.isEmailValid( "abc-d@mail.com"));
+        // Valid email address with letters and dash. Border case? Valid lenght of prefix and domain, with letter or digit after the -
+        assertTrue(BankAccount.isEmailValid( "abc-d@m.com"));
+        // Valid email with a 1 character domain, checking format. Border case? The domain portion consist of a 1 letter domain.
+        assertTrue(BankAccount.isEmailValid( "abc.def@mail-archive.com"));
+        //Valid email with period and dash in both prefix and domain. Border case? More complex domain, but still valid.
+        assertTrue(BankAccount.isEmailValid( "abc.def@mail-archive.co"));
+        //Valid email address, format, has - and letters. Two letters in .co  .Border case? Valid .co , lenght of 2 which is lowest possible
+        
+        assertFalse( BankAccount.isEmailValid(""));
+         // Invalid email address format, Border? Completely empty string, clearly invalid
+        assertFalse( BankAccount.isEmailValid("abc..d@mail.com"));
+        // Invalid email format with double periods in the prefix. Extra Character. Border? Having period after a period is invalid.
+        assertFalse( BankAccount.isEmailValid("abc-@mail.com"));
+        // Invalid email format with a dash just before @. Border? Need a letter or digit after. - or _
+        assertFalse( BankAccount.isEmailValid(".abc@mail.com"));
+        // Invalid email format with a period at the beginning of the prefix. Border? Period cannot be the first character of the prefix.
+        assertFalse( BankAccount.isEmailValid("abc!d@mail.com"));
+        // Invalid email format with invalid character. Border? Invalid character ! in the email.
+        
+        assertFalse( BankAccount.isEmailValid("abcmail.com"));
+        // Invalid email format, missing character. Border case: Missing @ symbol 
+        assertFalse( BankAccount.isEmailValid("abcdef@mail#arc.com"));
+        // Invalid email format with invalid character. Border? # is not allowed in domain 
+        assertFalse( BankAccount.isEmailValid("abcdef@mail..com"));
+        // Invalid email format.  Border? Two periods after the other in the domain name are not allowed.
+        assertFalse( BankAccount.isEmailValid("abcdef@mail"));
+        // Invalid email format with no (TLD) Border? Missing a TLD,no perid and two letters
+        assertFalse( BankAccount.isEmailValid("abcdef@mail.c"));
+        // Invalid email with invalid TLD format. Border? .com in TLD  must be at least 2 characters long 
+    }
+
+    
 
     @Test
     void constructorTest() {
