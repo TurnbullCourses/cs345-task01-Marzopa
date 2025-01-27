e0fc2090e9b9cad5acfd0a4b14a79971c3de40fd
Diana Lazo <dlazo@ithaca.edu>
2025-01-26 19:12:18 -0500

    added isemailvalidcode

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -40,11 +40,48 @@ public class BankAccount {
 
 
     public static boolean isEmailValid(String email){
+        //Diana Lazo
+        //used conditions below
+        // https://help.xmatters.com/ondemand/trial/valid_email_format.htm 
+        
+        // checks if and @ symbol is in the email
         if (email.indexOf('@') == -1){
             return false;
         }
-        else {
-            return true;
+
+        for (int i = 0; i < email.length(); i++) {
+            char c = email.charAt(i);
+            //checks if valid characters exist in the email
+            if (false == Character.isLetterOrDigit(c) && c != '.' && c != '-' && c != '@' && c != '_') {
+                return false; 
+            }
+            //checks to make sure digit is after certain symbols . - and _
+            if (c == '-' || c == '.' || c == '_') {
+                if (i == email.length() - 1 || false == Character.isLetterOrDigit(email.charAt(i + 1))) {
+                    return false;
+                }
+            }
         }
+        //checks lenght conditions of prefix and domain 
+        String[] split = email.split("@");
+        if (split.length != 2 ||  split[0].length() < 1 || split[1].length() < 3 || split[1].indexOf('.') == -1) {
+            return false;
+        }
+
+        //must start email with Letter or Digit
+        if (false == Character.isLetterOrDigit(split[0].charAt(0))){
+            return false;
+        }
+                   
+
+        //domain conditions
+        int lastperiod = split[1].lastIndexOf('.');
+        if (lastperiod == -1 || lastperiod == split[1].length() - 1 || split[1].length() - lastperiod - 1 < 2) {
+            return false;
+        }
+    
+        return true;
+
     }
+    
 }
